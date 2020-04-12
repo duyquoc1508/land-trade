0db81b170b477fe2e643884b38f911253915340d
duyquoc1508 <duyquoc.nguyen.bk@gmail.com>
2020-04-12 15:35:19 +0700

    feat: config cors insteads of cors package

--- server/src/config/middlewares.js
+++ server/src/config/middlewares.js
@@ -12,7 +12,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 // middlewares convert all field names on form to camelcase
 const camelcase = () => {
-  return function(req, res, next) {
+  return function (req, res, next) {
     req.body = camelcaseKeys(req.body, { deep: true });
     req.params = camelcaseKeys(req.params);
     req.query = camelcaseKeys(req.query);
@@ -20,12 +20,25 @@ const camelcase = () => {
   };
 };
 
-export default app => {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+export default (app) => {
+  app.use(bodyParser.json({ limit: "500kb" }));
+  app.use(bodyParser.urlencoded({ limit: "500kb", extended: true }));
   app.use(camelcase());
   app.use(passport.initialize());
-  app.use(cors({ origin: "http://localhost:8081" }));
+  // app.use(cors({ origin: "http://localhost:8081" }));
+
+  // Enable CORS from client-side
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Expose-Headers", "Content-Disposition");
+    next();
+  });
 
   if (isProd) {
     // app.use(compression());
