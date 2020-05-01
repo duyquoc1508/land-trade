7e72223f79c59fc3155b0b4d7c11faa48fdab5e6
duyquoc1508 <duyquoc.nguyen.bk@gmail.com>
2020-05-01 18:38:39 +0700

    feat (backend): add notification module

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -2,10 +2,12 @@ import userRoutes from "./user/user.route";
 import authRoutes from "./auth/auth.route";
 import certificationRoutes from "./certification/certification.route";
 import uploadRoutes from "./upload/upload.route";
+import notificationRoutes from "./notification/notification.route";
 
-export default app => {
+export default (app) => {
   app.use("/api/v1/upload", uploadRoutes);
   app.use("/api/v1/auth", authRoutes);
   app.use("/api/v1/users", userRoutes);
   app.use("/api/v1/certification", certificationRoutes);
+  app.use("/api/v1/notification", notificationRoutes);
 };
---
+++ server/src/api/notification/notification.controller.js
@@ -0,0 +1,33 @@
+import Notification from "./notification.model";
+
+// get notifications of user
+export async function getNotifications(req, res, next) {
+  try {
+    const pageSize = req.query.limit;
+    const begin = (req.query.page - 1) * pageSize;
+    const { publicAddress } = req.user;
+    const notifications = await Notification.find({
+      userAddress: publicAddress,
+    })
+      .lean()
+      .sort({ _id: -1 })
+      .limit(pageSize)
+      .skip(begin);
+    return res.status(200).json({ statusCode: 200, data: notifications });
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ * @dev create notification for TEST.
+ */
+export async function createNotification(req, res, next) {
+  try {
+    console.log(req.body);
+    const notification = await Notification.create(req.body);
+    return res.status(201).json({ statusCode: 201, data: notification });
+  } catch (error) {
+    next(error);
+  }
+}
