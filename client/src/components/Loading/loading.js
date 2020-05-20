62485f22ba24c4a8bdb34fabbec017ecd9508dcb
Tran Tan Loc <trantanloc279@tragmail.tragmail.tragmailcom>
2020-05-20 19:47:11 +0700

    done component loading

--- client/src/components/Loading/loading.js
+++ client/src/components/Loading/loading.js
@@ -1,61 +1,51 @@
 import React, { Component } from "react";
 
+// use <Loading isLoading={true} alert="Hello các bạn" />
 export default class Loading extends Component {
   render() {
     let isLoading = this.props.isLoading || false;
+    let alert = this.props.alert || "Vui lòng chờ ...";
     return (
       <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          style="margin: auto; background: rgb(241, 242, 243); display: block; shape-rendering: auto;"
-          width="214px"
-          height="214px"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="xMidYMid"
-        >
-          <circle
-            cx="50"
-            cy="50"
-            r="22"
-            stroke-width="3"
-            stroke="#0055a5"
-            stroke-dasharray="34.55751918948772 34.55751918948772"
-            fill="none"
-            stroke-linecap="round"
-            transform="rotate(311.492 50 50)"
-          >
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              dur="1.1904761904761905s"
-              repeatCount="indefinite"
-              keyTimes="0;1"
-              values="0 50 50;360 50 50"
-            ></animateTransform>
-          </circle>
-          <circle
-            cx="50"
-            cy="50"
-            r="18"
-            stroke-width="3"
-            stroke="#45aee7"
-            stroke-dasharray="28.274333882308138 28.274333882308138"
-            stroke-dashoffset="28.274333882308138"
-            fill="none"
-            stroke-linecap="round"
-            transform="rotate(-311.492 50 50)"
-          >
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              dur="1.1904761904761905s"
-              repeatCount="indefinite"
-              keyTimes="0;1"
-              values="0 50 50;-360 50 50"
-            ></animateTransform>
-          </circle>
-        </svg>
+        {isLoading ? (
+          <div>
+            <div
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                width: "100%",
+                height: "100%",
+                background: "rgba(0, 0, 0, 0.5)",
+                zIndex: 999999999999,
+              }}
+            ></div>
+            <img
+              src={`${process.env.REACT_APP_BASE_URL}/loading.png`}
+              style={{
+                position: "fixed",
+                top: "calc(50% - 107px)",
+                left: "calc(50% - 107px)",
+                zIndex: 9999999999,
+              }}
+            />
+            <h3
+              style={{
+                position: "fixed",
+                top: "calc(50% + 107px)",
+                left: "45%",
+                zIndex: 2147483647,
+                color: "#ffffff",
+              }}
+            >
+              {alert}
+            </h3>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
