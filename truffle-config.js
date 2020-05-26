ffbf43e445477985b7d0e08194aa5529840736f0
duyquoc1508 <duyquoc.nguyen.bk@gmail.com>
2020-05-26 16:55:30 +0700

    fix (contract): change datatype of property in smartcontract to string

--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,18 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  compilers: {
+    solc: {
+      version: "0.5.3"
+    }
+  },
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "45.32.124.84",
+      port: 8545,
       network_id: "*",
     },
-    test: {
+    ganache: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
