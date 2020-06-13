90412c60de8aa1b973bab7f398e177e8dd2a019e
duyquoc1508 <duyquoc.nguyen.bk@gmail.com>
2020-06-13 10:46:15 +0700

    fix(client): init transaction

--- client/src/modules/InitTransaction/action.js
+++ client/src/modules/InitTransaction/action.js
@@ -1,5 +1,5 @@
 import { INIT_TRANSACTION_REQUEST } from "./constants";
 
-export function initTransactionRequest(data) {
-  return { type: INIT_TRANSACTION_REQUEST, payload: data };
+export function initTransactionRequest({ history, data }) {
+  return { type: INIT_TRANSACTION_REQUEST, payload: { history, data } };
 }
