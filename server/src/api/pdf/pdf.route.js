0a4ba6a0c00946079675e6f61eea6395bb434538
Tran Tan Loc <trantanloc279@tragmail.tragmail.tragmailcom>
2020-06-14 11:10:29 +0700

    api render pdf down-payment

---
+++ server/src/api/pdf/pdf.route.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import path from "path";
+let ejs = require("ejs");
+let pdf = require("html-pdf");
+
+const routes = Router();
+
+routes.post("/down-payment", (req, res) => {
+  let data = req.body;
+  ejs.renderFile(
+    path.join(__dirname, "/template/down-payment-pdf.ejs"),
+    data,
+    (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        let options = {
+          height: "10in",
+          width: "8.5in",
+          header: {
+            height: "20mm",
+          },
+          footer: {
+            height: "20mm",
+          },
+        };
+        let fileName = `down-payment-${Date.now()}.pdf`;
+        pdf
+          .create(data, options)
+          .toFile(`./src/public/pdf/${fileName}`, function (err, data) {
+            if (err) {
+              res.send(err);
+            } else {
+              res.json({
+                message: "File created successfully",
+                url: `/static/pdf/${fileName}`,
+              });
+            }
+          });
+      }
+    }
+  );
+});
+
+export default routes;
