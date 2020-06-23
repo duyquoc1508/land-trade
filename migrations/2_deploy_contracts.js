faa426ef5c672ee5eb1f840eb2ee90a91d332d9b
duyquoc1508 <duyquoc.nguyen.bk@gmail.com>
2020-06-23 20:13:17 +0700

    feat(contract): add transaction contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 const RoleBasedAcl = artifacts.require("./RoleBasedAcl.sol");
 const RealEstate = artifacts.require("./RealEstate.sol");
+const Transaction = artifacts.require("./Transaction.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(RoleBasedAcl).then(function () {
-    return deployer.deploy(RealEstate, RoleBasedAcl.address);
+    return deployer.deploy(RealEstate, RoleBasedAcl.address).then(function () {
+      return deployer.deploy(Transaction, RealEstate.address);
+    });
   });
 };
